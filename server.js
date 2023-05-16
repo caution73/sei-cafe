b29f14e422e9ea20f46e1afdb66d64beba329ee9
Devin Hansen <hansdevi@isu.edu>
2023-05-16 14:13:37 -0500

    MERN stack infrastructure.

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config(); // this needs to be on line 1
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan');
+require("./config/database")
+
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// Configure both serve favicon and 
+// 
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Middleware to verify token and assign user object of payload to req.user.
+// Be sure to mount before routes
+app.use(require('./config/checkToken'));
+
+
+// This block of code is for testing when setting up your server and such.
+// app.get('/test', (req, res) => {
+//     res.json({ message: 'Hello im the server' });
+//   });
+
+
+app.use('/api/users', require("./routes/api/users"))
+
+
+// 'Catch all' route. (the * is necessary). This 
+// is necessary to return the index.html on all 
+// non-AJAX requests.
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+// Configure to use port 3001 instead of 3000 during development
+// to avoid collision with React's dev server.
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+})
+    
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import NewOrderPage from "./pages/NewOrder/NewOrderPage.js";
+import AuthPage from "./pages/Auth/AuthPage.js";
+import OrderHistoryPage from "./pages/OrderHistory/OrderHistoryPage";
+import NavBar from "./components/NavBar/NavBar";
+import { getUser } from "./utilities/users-service";
 
 function App() {
+  // This code is for testing when setting up server and such.
+  // async function testCall () {
+  //   const res = await fetch("/test")
+  //   const data = await res.json()
+  //   console.log(data)
+  // }
+
+  // useEffect(() => {
+  //   testCall()
+  // }, [])
+  const [user, setUser] = useState(getUser());
+  console.log('current user', user)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <div>
+          <NavBar user={user} setUser={setUser}/>
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </div>
+      ) : (
+        <AuthPage setUser={setUser}/>
+      )}
     </div>
   );
 }
