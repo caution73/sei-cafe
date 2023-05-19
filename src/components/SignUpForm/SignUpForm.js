d8bb6989ed2ccbe6e864d3e325fce99d281535bf
Devin Hansen <hansdevi@isu.edu>
2023-05-19 10:27:41 -0500

    Finished SEI cafe.

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,89 +1,62 @@
 import { Component } from "react";
-import { signUp } from '../../utilities/users-service'
+import { signUp } from '../../utilities/users-service';
 
 export default class SignUpForm extends Component {
-  state = {
-    name: "",
-    email: "",
-    password: "",
-    confirm: "",
-    error: "",
-  };
+state = {
+  name: '',
+  email: '',
+  password: '',
+  confirm: '',
+  error: ''
+};
 
-  // The object passed to setState is merged with the current state object
-  handleChange = (evt) => {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-      error: "",
-    });
-  };
+handleChange = (evt) => {
+  this.setState({
+    [evt.target.name]: evt.target.value,
+    error: ''
+  });
+};
 
-  handleSubmit = async (evt) => {
-    evt.preventDefault();
-    try {
-      // We don't want to send the 'error' or 'confirm' property,
-      //  so let's make a copy of the state object, then delete them
-      const formData = { ...this.state };
-      delete formData.error;
-      delete formData.confirm;
-      // The promise returned by the signUp service method
-  // will resolve to the user object included in the
-  // payload of the JSON Web Token (JWT)
-  const user = await signUp(formData);
-  // Baby step!
-  console.log(user)
-  this.props.setUser(user)    
-  //throw new Error() // Using this to test the "catch"
-    } catch (err) {
-      this.setState({ error: "Sign-up failed. Please try again." });
-    }
-  };
-
-  render() {
-    const disable = this.state.password !== this.state.confirm;
-    return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Confirm</label>
-            <input
-              type="password"
-              name="confirm"
-              value={this.state.confirm}
-              onChange={this.handleChange}
-              required
-            />
-            <button type="submit" disabled={disable}>
-              SIGN UP
-            </button>
-          </form>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
-      </div>
-    );
+handleSubmit = async (evt) => {
+  evt.preventDefault();
+  try {
+    const formData = {...this.state};
+    delete formData.confirm;
+    delete formData.error;
+    // The promise returned by the signUp service method
+    // will resolve to the user object included in the
+    // payload of the JSON Web Token (JWT)
+    const user = await signUp(formData);
+    // Baby step
+    this.props.setUser(user);
+  } catch {
+    // An error happened on the server
+    this.setState({ error: 'Sign Up Failed - Try Again' });
   }
+};
+
+// We must override the render method
+// The render method is the equivalent to a function-based component
+// (its job is to return the UI)
+render() {
+  const disable = this.state.password !== this.state.confirm;
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <label>Name</label>
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+          <label>Email</label>
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+          <label>Password</label>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+          <label>Confirm</label>
+          <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+          <button type="submit" disabled={disable}>SIGN UP</button>
+        </form>
+      </div>
+      <p className="error-message">&nbsp;{this.state.error}</p>
+    </div>
+  );
+}
 }
